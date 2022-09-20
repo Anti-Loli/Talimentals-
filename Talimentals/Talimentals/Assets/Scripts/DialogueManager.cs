371a9a38a3fe16d50cefe2f761bb8b17907d6a65
Hunter Hockman <84150506+Anti-Loli@users.noreply.github.com>
2022-09-20 14:07:27 -0400

    NPC Object, Text box beta, and gitignore

---
+++ Talimentals/Talimentals/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    public NPC npc;//npc scriptable object
+
+    //I'll leave the rest to you Anthony
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
