638681350f99ae15bb56ba79158729ed18a6f3c3
Hunter Hockman <84150506+Anti-Loli@users.noreply.github.com>
2022-10-04 10:41:35 -0400

    Created talimental scriptable object
    also gusto

---
+++ Talimentals/Talimentals/Assets/Talimentals/Talimental.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Talimental file", menuName = "Talimental files archive")]
+public class Talimental : ScriptableObject
+{
+    public string name;//name of the talimental
+    public int health;
+    public int attack;
+    public int defence;
+    public int speed;
+    public string element;
+    [TextArea(3, 15)]
+    public string[] moves;
+    
+}
