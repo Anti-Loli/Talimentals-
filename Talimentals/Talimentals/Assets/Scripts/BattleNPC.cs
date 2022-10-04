b100bb45b82db6e4f8a8fd0337a1d2398b348aef
mediomediocre <89039135+mediomediocre@users.noreply.github.com>
2022-10-04 10:33:17 -0400

    state machine functionality
    deez nuts

--- Talimentals/Talimentals/Assets/Scripts/BattleNPC.cs
+++ Talimentals/Talimentals/Assets/Scripts/BattleNPC.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class BattleNPC : MonoBehaviour
 {
+    private enum State { Normal, Battle };
+    State currentState;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(NormalState());
     }
 
     // Update is called once per frame
@@ -16,13 +19,48 @@ public class BattleNPC : MonoBehaviour
         
     }
 
+    void ChangeState(State newState)
+    {
+        StopAllCoroutines();
+        currentState = newState;
+
+        switch (currentState)
+        {
+            case State.Normal:
+                StartCoroutine(NormalState());
+                break;
+            case State.Battle:
+                StartCoroutine(BattleState());
+                break;
+        } 
+    }
+
+    IEnumerator NormalState()
+    {
+        Debug.Log("i'm very normal right now (in normal state)");
+        yield return null;
+    }
+
+    IEnumerator BattleState()
+    {
+        Debug.Log("i must kill (in battle state)");
+        yield return null;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.CompareTag("Player"))
         {
             Debug.Log("works");
+            ChangeState(State.Battle);
             //change state machine code
         }
+    }
+
+    //temporary?
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        ChangeState(State.Normal);
     }
 
     //state machine code
