05db8f3009c489da66fcacc4ffed4751c75eb44b
Hunter Hockman <84150506+Anti-Loli@users.noreply.github.com>
2022-10-25 10:43:11 -0400

    Updated the battle system
    Go first mechanic and tristans changes

--- Assets/BattleSystem/scripts/BattleSystem.cs
+++ Assets/BattleSystem/scripts/BattleSystem.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
@@ -23,6 +24,8 @@ public class BattleSystem : MonoBehaviour
 	public BattleHUD1 enemyHUD;
 
 	public BattleState state;
+
+	public GameObject attackMenu;
 
 	// Start is called before the first frame update
 	void Start()
@@ -65,6 +68,10 @@ public class BattleSystem : MonoBehaviour
 			state = BattleState.WON;
 			EndBattle();
 		}
+		else if(playerUnit.speed < enemyUnit.speed)
+        {
+			PlayerTurn();
+		}
 		else
 		{
 			state = BattleState.ENEMYTURN;
@@ -89,6 +96,10 @@ public class BattleSystem : MonoBehaviour
 			state = BattleState.LOST;
 			EndBattle();
 		}
+		else if(playerUnit.speed < enemyUnit.speed)
+        {
+			StartCoroutine(PlayerAttack());
+		}
 		else
 		{
 			state = BattleState.PLAYERTURN;
@@ -102,16 +113,19 @@ public class BattleSystem : MonoBehaviour
 		if (state == BattleState.WON)
 		{
 			dialogueText.text = "You won the battle!";
+			SceneManager.LoadScene("Hunters Dev Room");
 		}
 		else if (state == BattleState.LOST)
 		{
 			dialogueText.text = "You were defeated.";
+			SceneManager.LoadScene("Hunters Dev Room");
 		}
 	}
 
 	void PlayerTurn()
 	{
 		dialogueText.text = "Choose an action:";
+		attackMenu.SetActive(true);
 	}
 
 	IEnumerator PlayerHeal()
@@ -132,14 +146,24 @@ public class BattleSystem : MonoBehaviour
 		if (state != BattleState.PLAYERTURN)
 			return;
 
-		StartCoroutine(PlayerAttack());
+		attackMenu.SetActive(false);
+
+		//compares the speeds of the player and enemy and decides which one goes first 
+		if (playerUnit.speed > enemyUnit.speed)
+        {
+			StartCoroutine(PlayerAttack());
+		}
+		else if(playerUnit.speed < enemyUnit.speed)
+        {
+			StartCoroutine(EnemyTurn());
+		}
 	}
 
 	public void OnHealButton()
 	{
 		if (state != BattleState.PLAYERTURN)
 			return;
-
+		attackMenu.SetActive(false);
 		StartCoroutine(PlayerHeal());
 	}
 
