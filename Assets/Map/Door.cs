a57520197def6113e88df3623b86f7e56fc011af
Hunter Hockman <84150506+Anti-Loli@users.noreply.github.com>
2022-11-15 10:31:16 -0500

    Updated door script and prefab

--- Assets/Map/Door.cs
+++ Assets/Map/Door.cs
@@ -6,32 +6,45 @@ using UnityEngine.SceneManagement;
 public class Door : MonoBehaviour
 {
     Scene currentScene;
+    bool inCollider;
 
     // Start is called before the first frame update
     void Start()
     {
         currentScene = SceneManager.GetActiveScene();
+        inCollider = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(inCollider)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (currentScene.name == "InsideSchool")
+                {
+                    SceneManager.LoadScene("Cave");
+                }
+                else if (currentScene.name == "OutsideSchool")
+                {
+                    SceneManager.LoadScene("Credits");
+                }
+                else if (currentScene.name == "Cave")
+                {
+                    SceneManager.LoadScene("OutsideSchool");
+                }
+            }
+        }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (currentScene.name == "InsideSchool")
-        {
-            SceneManager.LoadScene("Cave");
-        }
-        else if (currentScene.name == "OutsideSchool")
-        {
-            SceneManager.LoadScene("Credits");
-        }
-        else if (currentScene.name == "Cave")
-        {
-            SceneManager.LoadScene("OutsideSchool");
-        }
+        inCollider = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        inCollider = false;
     }
 }
